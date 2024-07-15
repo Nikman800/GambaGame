13af03449f4d268ee0fc4d72119dd653098eee85
Nikman800 <76964182+Nikman800@users.noreply.github.com>
2024-07-15 11:10:59 +0530

    Added some buttons to the homepage

--- GambaGame/src/client/App.tsx
+++ GambaGame/src/client/App.tsx
@@ -7,6 +7,16 @@ function App() {
 
   return (
     <div className="App">
+      <header className="App-header">
+        <div className="top-right">
+          <button className="login-button">Log In</button>
+        </div>
+        <h1 style={{ fontFamily: 'Silkscreen, cursive' }}>GambaGame</h1>
+        <div className="buttons">
+          <button className="bracket-button">Join Bracket</button>
+          <button className="bracket-button">Create Bracket</button>
+        </div>
+      </header>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -15,7 +25,6 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
